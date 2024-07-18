4f780be4596817d00b96dbca0ed57088a6f38510
tranhoa2602 <thdblacksky@gmail.com>
2024-07-18 09:14:33 +0700

    Update: create new code

---
+++ src/assets/Components/Login/authService.js
@@ -0,0 +1,74 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { ref, set, get } from "firebase/database";
+import { auth, database } from "../../../firebaseConfig";
+
+// export const signUp = async (email, password, role) => {
+//   try {
+//     const userCredential = await createUserWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     await set(ref(database, "users/" + user.uid), {
+//       email: email,
+//       role: role,
+//     });
+//     return user;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+// export const logIn = async (email, password) => {
+//   try {
+//     const userCredential = await signInWithEmailAndPassword(
+//       auth,
+//       email,
+//       password
+//     );
+//     const user = userCredential.user;
+//     const snapshot = await get(ref(database, "users/" + user.uid));
+//     const userData = snapshot.val();
+//     console.log("user", user);
+//     console.log("userData", userData);
+//     return { user, userData };
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+export const signUp = async (email, password, role) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    await set(ref(database, "users/" + user.uid), { email, role });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Hàm đăng nhập người dùng hiện tại
+export const logIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    const snapshot = await get(ref(database, "users/" + user.uid));
+    const userData = snapshot.val();
+    return { user, userData };
+  } catch (error) {
+    throw error;
+  }
+};
